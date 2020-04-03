e23658b9a24a3c57d0cb7240430e6db369b9868c
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-04-03 14:19:50 +0800

    Format Chapter.vue code

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -5,7 +5,9 @@
         >上一章</router-link
       >
       <router-link to="/">目录</router-link>
-      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < chapterCount"
+      <router-link
+        :to="`/chapter/${chapterNum + 1}`"
+        v-if="chapterNum < chapterCount"
         >下一章</router-link
       >
     </div>
@@ -17,7 +19,9 @@
         >上一章</router-link
       >
       <router-link to="/">目录</router-link>
-      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < chapterCount"
+      <router-link
+        :to="`/chapter/${chapterNum + 1}`"
+        v-if="chapterNum < chapterCount"
         >下一章</router-link
       >
     </div>
@@ -57,9 +61,9 @@ export default {
 
   data() {
     return {
-      chapterCount: 1
+      chapterCount: 1,
     }
-  }
+  },
 }
 </script>
 
