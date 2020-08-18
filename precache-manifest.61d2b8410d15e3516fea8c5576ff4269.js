5f4fd572e2ba7cba79aeeb649363c049aae48da4
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-08-18 15:06:45 +0800

    deploy

---
+++ precache-manifest.61d2b8410d15e3516fea8c5576ff4269.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf4ca9295eb3b2a8fc8",
+    "url": "css/app.6cfe2629.css"
+  },
+  {
+    "revision": "561aad8bbdd3cc989597c0f3b03c279a",
+    "url": "img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "1d79fbb32a868a5301da8edb0370662b",
+    "url": "img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "561aad8bbdd3cc989597c0f3b03c279a",
+    "url": "img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "1d79fbb32a868a5301da8edb0370662b",
+    "url": "img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "4c0f567e4e9ef1db76c8a0cfe7cf925e",
+    "url": "img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "36465ea3e5dfc5c1cef7c66ddc611316",
+    "url": "img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "218b5ea68429685255e9e8e073e28c75",
+    "url": "img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "d7fd36c0b652d1d449fdb8cbccac833c",
+    "url": "img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "7a318e9d8118526e0ee7aa3522f981fb",
+    "url": "img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "218b5ea68429685255e9e8e073e28c75",
+    "url": "img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "a0d97fd8a12647659fd2af68ca6c6bca",
+    "url": "img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "0543d06a5f040d7f38c81d3be6104b9b",
+    "url": "img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "73dbaf95a9fe7779ab6dcf1ab48bd21a",
+    "url": "img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "50adc9e528b0156b4b47f22eb071c400",
+    "url": "img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "8838ed08c8cb079876389172e47dc046",
+    "url": "img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "19ce18e7b6c5d357037cff63a84867bf",
+    "url": "index.html"
+  },
+  {
+    "revision": "4cf4ca9295eb3b2a8fc8",
+    "url": "js/app.dfaa70db.js"
+  },
+  {
+    "revision": "3a7bfcfd24c995fe8a65",
+    "url": "js/chunk-2d217a99.7c4a50aa.js"
+  },
+  {
+    "revision": "4b32702470e3424b5e6d",
+    "url": "js/chunk-vendors.eb1651d2.js"
+  },
+  {
+    "revision": "392018d457ab2126aba441f9189c8930",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
